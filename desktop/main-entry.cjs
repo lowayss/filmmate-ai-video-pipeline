411731c57b89b55d236c33d3e7e0b0ccf6228c26
lowayss <loways@naver.com>
2026-09-03 13:59:16 +0900

    feat(vcam): install Visual AR V3 runtime

--- desktop/main-entry.cjs
+++ desktop/main-entry.cjs
@@ -1,3 +1,5 @@
 const {installVirtualCamera} = require("./virtual-camera-main.cjs");
+const {installVisualCamera} = require("./virtual-camera-visual-main.cjs");
 installVirtualCamera();
+installVisualCamera();
 require("./main.cjs");
