d934562ac49e1534afe8cabdd3b0421d7c5695d1
lowayss <loways@naver.com>
2026-09-03 16:13:45 +0900

    revert: remove FreezeFrame VCAM work from FilmMate

--- desktop/main-entry.cjs
+++
@@ -1,7 +0,0 @@
-const {installVirtualCamera} = require("./virtual-camera-main.cjs");
-const {installVisualCamera} = require("./virtual-camera-visual-main.cjs");
-const {installVirtualCameraStage} = require("./virtual-camera-stage-main.cjs");
-installVirtualCamera();
-installVisualCamera();
-installVirtualCameraStage();
-require("./main.cjs");
