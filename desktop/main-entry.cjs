c106fe03b65f4248d0cc47e0b7a65ee9f3be9803
lowayss <loways@naver.com>
2026-09-03 16:05:33 +0900

    feat(vcam): install stage V4 main bridge

--- desktop/main-entry.cjs
+++ desktop/main-entry.cjs
@@ -1,5 +1,7 @@
 const {installVirtualCamera} = require("./virtual-camera-main.cjs");
 const {installVisualCamera} = require("./virtual-camera-visual-main.cjs");
+const {installVirtualCameraStage} = require("./virtual-camera-stage-main.cjs");
 installVirtualCamera();
 installVisualCamera();
+installVirtualCameraStage();
 require("./main.cjs");
