07dffb1935e6bbacb05ab5ffbb809fbf74f47961
lowayss <loways@naver.com>
2026-09-03 16:53:44 +0900

    feat(vcam): install live 3D blockout service

--- desktop/main-entry.cjs
+++ desktop/main-entry.cjs
@@ -1,5 +1,7 @@
 const {installVirtualCamera} = require("./virtual-camera-main.cjs");
 const {installVisualCamera} = require("./virtual-camera-visual-main.cjs");
+const {installVirtualCameraBlockout} = require("./virtual-camera-blockout-main.cjs");
 installVirtualCamera();
 installVisualCamera();
+installVirtualCameraBlockout();
 require("./main.cjs");
